042064bb41ad483a0018eb2f027e932818387f74
meilunzhi@gmail.com <meilunzhi@gmail.com>
2017-12-01 18:33:39 +0800

    add show page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('users','UsersController',['only' =>['show','update','edit']]);
