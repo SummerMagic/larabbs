4ccd98a6401c059545b11d9bbdb5b3768ac1ecdf
root <root@homestead>
2017-12-01 10:12:30 +0000

    add auth or register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<title>@yield('title', 'LaraBBS') - lara-bbs</title>
+	<title>@yield('title', 'cshome') - cshome</title>
 
 	<!-- Styles -->
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
